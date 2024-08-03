0b0e8190a514788104c287002c732f5769be6ca8
WiSpGarrett <garrett.w@northeastern.edu>
2024-08-03 12:05:51 -0400

    Changes by adambrenner

--- src/main/java/group8/controller/MainController.java
+++ src/main/java/group8/controller/MainController.java
@@ -1,5 +1,42 @@
 package group8.controller;
 
+import group8.model.ITriviaCollection;
+import group8.model.helpers.APIUtils;
+
 public class MainController {
-    
+
+    private ITriviaCollection user;
+    private ITriviaCollection api;
+
+    public MainController(ITriviaCollection userCollection, ITriviaCollection apiCollection) throws Exception {
+
+        this.user = userCollection;
+        this.api = apiCollection;
+
+        try {
+            APIUtils.requestToken();
+            APIUtils.requestCategories();
+        } catch (Exception e) {
+            e.getCause();
+        }
+
+    }
+
+    //     Generate API List
+
+    public void GenerateAPIList() throws Exception {}
+
+
+// ````````````Listeners`````````
+//     Generate API List
+//     Load
+//     Save
+//     Sort
+//     Filter
+//     MoveToUser
+//     MoveToApi
+//     Reset Filter
+
+
+
 }
