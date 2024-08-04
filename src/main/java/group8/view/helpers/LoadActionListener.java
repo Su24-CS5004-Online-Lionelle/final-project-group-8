96b4250b64d415081a35eeed7f4b7ddbc2b9a791
yaguang99 <72411448+yaguang99@users.noreply.github.com>
2024-08-04 16:16:24 -0400

    Modifying the Load button to work with FileUtilities

--- src/main/java/group8/view/helpers/LoadActionListener.java
+++ src/main/java/group8/view/helpers/LoadActionListener.java
@@ -1,10 +1,16 @@
 package group8.view.helpers;
 
+import group8.model.FileUtilities;
 import group8.model.TriviaQuestion;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Action listener for loading the user collection from a file.
@@ -31,9 +37,34 @@ public class LoadActionListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         JFileChooser fileChooser = new JFileChooser();
+        // Restricts user to select JSON files only
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setFileFilter(new javax.swing.filechooser.
+                FileNameExtensionFilter("JSON Files", "json"));
+
         int result = fileChooser.showOpenDialog(null);
+
         if (result == JFileChooser.APPROVE_OPTION) {
-            // Logic to load the user collection from the selected file
+            File selectedFile = fileChooser.getSelectedFile();
+            String filePath = selectedFile.getAbsolutePath();
+
+            try {
+                List<TriviaQuestion> loadedQuestions = FileUtilities.loadTrivia(filePath);
+
+
+                // Testing function
+            System.out.println("Loaded objects via LoadedQuestions");
+            for (TriviaQuestion q : loadedQuestions) {
+                System.out.println(q.question());
+            }
+                // Testing function
+
+
+                // Display function here.
+
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, "Error loading trivia: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 }
