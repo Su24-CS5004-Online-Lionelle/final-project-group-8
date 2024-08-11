91a67b1e948bd5d02d661ea8198187fed4e34b2f
WiSpGarrett <garrett.w@northeastern.edu>
2024-08-11 11:29:47 -0400

    Additions to javadocs

--- src/main/java/group8/view/helpers/MoveToActionListener.java
+++ src/main/java/group8/view/helpers/MoveToActionListener.java
@@ -9,12 +9,42 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+/**
+ * MoveToActionListener handles the action of moving a selected TriviaQuestion
+ * from one list to another (API list to User list or vice versa).
+ */
 public class MoveToActionListener implements ActionListener {
+
+    /**
+     * The main view of the application.
+     */
     private MainView view;
+
+    /**
+     * The controller managing the interaction between the view and the model.
+     */
     private MainController controller;
+
+    /**
+     * The list from which the TriviaQuestion is selected.
+     */
     private JList<TriviaQuestion> sourceList;
+
+    /**
+     * Determines whether the selected TriviaQuestion is moving to the user list or the API list.
+     * If true, the question is moved to the user list; if false, it is moved to the API list.
+     */
+
     private boolean moveToUser; // true if moving to user, false if moving to API
 
+    /**
+     * Constructs a MoveToActionListener with the specified parameters.
+     *
+     * @param view       the main view of the application.
+     * @param controller the controller managing the interaction between the view and the model.
+     * @param sourceList the list from which the TriviaQuestion is selected.
+     * @param moveToUser true if the question is being moved to the user list, false if to the API list.
+     */
     public MoveToActionListener(MainView view, MainController controller, JList<TriviaQuestion> sourceList, boolean moveToUser) {
         this.view = view;
         this.controller = controller;
@@ -22,6 +52,12 @@ public class MoveToActionListener implements ActionListener {
         this.moveToUser = moveToUser;
     }
 
+    /**
+     * Handles the action of moving the selected TriviaQuestion from one list to another.
+     * Updates the relevant list models and re-applies filters and sorting.
+     *
+     * @param e the action event that triggered this method.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         TriviaQuestion selectedQuestion = sourceList.getSelectedValue();
